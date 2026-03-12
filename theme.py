c486a99c5ba5ae423abd40d7367aa3178007ae44
Tong-ST <145736642+Tong-ST@users.noreply.github.com>
2026-03-13 00:51:47 +0700

    Update theme

--- theme.py
+++ theme.py
@@ -1,11 +1,11 @@
 # CoreOS Theme Settings
 
 THEME = {
-    "text": "#FFB642",
-    "border": "rgba(92, 61, 11, 0.8)",
-    "accent": "#FFB642",
+    "text": "rgba(86, 231, 151, 0.7)",
+    "border": "rgba(86, 231, 151, 0.15)",
+    "accent": "#41ec55",
     "background": "transparent",
-    "surface": "rgba(92, 61, 11, 0.2)",  # Used for modals
+    "surface": "#001500",  # Used for modals
     
     # Layout settings
     "stats_row_height": 9,
